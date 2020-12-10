f819a08dc054a013ea7ccdc640c4f1114cd20abd
Kartik B Bhargav <kartikbhargav93@gmail.com>
2020-12-10 14:31:03 +0100

    Adding the code change suugested by Humberto

--- VolVis/src/gui/RaycastRendererPanel.java
+++ VolVis/src/gui/RaycastRendererPanel.java
@@ -390,7 +390,7 @@ public class RaycastRendererPanel extends javax.swing.JPanel {
     }//GEN-LAST:event_cuttingPlaneCheckboxActionPerformed
 
     private void IsovalueTextBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_IsovalueTextBox1ActionPerformed
-        String s = this.IsovalueTextBox.getText();
+        String s = this.IsovalueTextBox1.getText();
         renderer.setIsoValueBack(Float.parseFloat(s));//set value
         renderer.changed();
     }//GEN-LAST:event_IsovalueTextBox1ActionPerformed
